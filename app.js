ca1beaf41dda81a91c89199a0f942e700b4a004d
Ccrazydreamer <bcrazydreamer@gmail.com>
2018-06-09 03:09:57 +0530

    a

--- app.js
+++ app.js
@@ -3,18 +3,22 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require("path");
+app.use(express.static(path.join(__dirname,"public")));
 var port = process.env.PORT || 3000;
 
 
 app.get("/",function(req,res)
 	{
-		res.status(300).sendFile(path.join(__dirname,"test.html")); 
+		res.status(300).sendFile(path.join(__dirname,"index.html")); 
 	});
 app.get("/fb",function(req,res)
 	{
 		res.status(300).sendFile(path.join(__dirname,"fb.html")); 
 	});
-
+app.get("/test",function(req,res)
+	{
+		res.status(300).sendFile(path.join(__dirname,"test.html")); 
+	});
 io.on('connection', function(socket){
 //------------------------------------------------------------------------
   socket.on('facebook', function(code){
