54b25a5c8f87d24b76f244f3073c4e34687c3619
Ccrazydreamer <bcrazydreamer@gmail.com>
2018-06-09 02:42:15 +0530

    a

--- app.js
+++ app.js
@@ -23,8 +23,5 @@ io.on('connection', function(socket){
   socket.on('test', function(test){
     console.log(test);
   });
-  socket.on('pic', function(test){
-    console.log("--------------------------"+test);
-  });
 });
 http.listen(port);
